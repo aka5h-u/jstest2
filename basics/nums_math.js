413fe2f8a035e01b1f36b758ca0208c396999388
AKASH U <akashu97@gmail.com>
2025-01-11 16:30:00 +0000

    math functions

--- basics/nums_math.js
+++ basics/nums_math.js
@@ -13,3 +13,21 @@ console.log(otherNum.toPrecision(2))
 
 const hundred = 10000000
 console.log(hundred.toLocaleString('en-IN'))
+
+//MAth library
+let numNeg = -4
+console.log(Math.abs(numNeg))
+
+console.log(Math.round(22.59))
+console.log(Math.ceil(4.1))
+console.log(Math.floor(4.9))
+
+console.log(Math.min(3,1,0,-9))
+
+//math random
+console.log((Math.random() ) )
+
+let min = 1
+let max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
