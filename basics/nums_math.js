c8217600088543241304e6637748584826d1f564
AKASH U <akashu97@gmail.com>
2025-01-11 07:33:53 +0000

    numbersjs

---
+++ basics/nums_math.js
@@ -0,0 +1,15 @@
+const num = 400
+console.log(num)
+
+const num2 = new Number(100)
+console.log(num2)
+
+console.log(num2.toString().length)
+
+console.log(num2.toFixed(2))
+
+const otherNum = 23.956
+console.log(otherNum.toPrecision(2))
+
+const hundred = 10000000
+console.log(hundred.toLocaleString('en-IN'))
