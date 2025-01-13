e3aaf843f49d9f2a62c902f3bd0c812015fd11b8
AKASH U <akashu97@gmail.com>
2025-01-13 17:58:15 +0000

    array methds

--- basics/arrays.js
+++ basics/arrays.js
@@ -29,3 +29,33 @@ const n2 = arr.splice(1,3)
 console.log(n2)
 
 console.log('C', arr)
+
+
+const marvel = ["Ironman", "Thor", "Hulk"]
+const dc = ["Batman", "Flash"]
+
+// marvel.push(dc)
+// console.log(marvel)
+
+// const heros = marvel.concat(dc)
+// console.log(heros)
+
+//spread values over array
+ 
+const heros = [...marvel, ...dc]
+console.log(heros)
+
+const myArr = [1,2,3,[4,5,6], 7, 8,[7,8]]
+const myArr2 = myArr.flat(1)
+console.log(myArr2)
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Akash"))
+
+console.log(Array.from({name : "Akash"})) //object converted to array gives an empty array
+
+let score1 = 400
+let score2 = 555
+let score3  =1000
+
+console.log(Array.of(score1, score2, score3))
